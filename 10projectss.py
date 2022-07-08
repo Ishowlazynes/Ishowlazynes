571bbff950f604487793c6c94dad29b7b989ad42
Ishowlazynes <108705057+Ishowlazynes@users.noreply.github.com>
2022-07-08 15:41:52 -0400

    Update 10projectss.py

--- 10projectss.py
+++ 10projectss.py
@@ -1,3 +1,5 @@
+
+
 def Resgister():
     Info = open('readme.txt', 'r')
     username = input("Enter Username: ")
@@ -7,5 +9,26 @@ def Resgister():
     if Password != Password1:
         print("Passwords don't match. Please Try again.")
         Resgister()
+    else:
+        if len(Password) <= 6:
+            print("Password is too short")
+            Resgister()
+        elif username in Info:
+            print("Username exist")
+            Resgister()
+        else:
+            Info = open('readme.txt', 'a')
+            Info.writelines(username + ", " + Password + "\n")
+            print("Success")
+
+
 
 Resgister()
+
+def login():
+    Login_Info = open('readme.txt', 'r')
+    username = input("Enter your Username: ")
+    password = input("Enter your password: ")
+    Login_Info.readline()
+def access():
+    pass
