33ebf61ab7a05c9676a562888e755730af187933
Ishowlazynes <108705057+Ishowlazynes@users.noreply.github.com>
2022-07-11 17:16:39 -0400

    Update 10projectss.py

--- 10projectss.py
+++ 10projectss.py
@@ -1,34 +1,54 @@
 
 
-def Resgister():
-    Info = open('readme.txt', 'r')
+def resgister():
+    db = open("readme.txt", "r")
     username = input("Enter Username: ")
     Password = input("Enter Password: ")
     Password1 = input("Please Confirm Password: ")
-
+    d = []
+    f = []
+    #To see the username and passwords
+    for i in db:
+        a,b = i.split(", ")
+        b = b.strip()
+        d.append(a) # seperates the usernames and passwords
+        f.append(b)
+    data = dict(zip(d, f))
+    print(data)
+    # Checks prints the login info
     if Password != Password1:
         print("Passwords don't match. Please Try again.")
-        Resgister()
+        resgister()
     else:
         if len(Password) <= 6:
-            print("Password is too short")
-            Resgister()
-        elif username in Info:
+           print("Password is too short")
+           resgister()
+        elif username in d:
             print("Username exist")
-            Resgister()
+            resgister()
         else:
-            Info = open('readme.txt', 'a')
-            Info.writelines(username + ", " + Password + "\n")
+            db = open('readme.txt', 'a')
+            db.write(username + ", " + Password + "\n")
             print("Success")
+            #If their problem with resgister or it will be success
+#resgister()
+
+def Sign_in():
+    DB = open('readme.txt', 'r')
+    Username = input("Enter Username here: ")
+    Password = input("Enter Password here: ")
+    d = []
+    f = []
+
+    for i in DB:
+        a,b = i.split(", ")
+        b = b.strip()
+        d.append(a)
+        f.append(b)
+    data = dict(zip(d, f))
+    print(data)
 
 
-
-Resgister()
-
-def login():
-    Login_Info = open('readme.txt', 'r')
-    username = input("Enter your Username: ")
-    password = input("Enter your password: ")
-    Login_Info.readline()
+Sign_in()
 def access():
     pass
